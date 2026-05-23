c4b5699d186808bfe332821dcf6959251bb583a2
ovunceker <ozguneker6776@gmail.com>
2026-05-23 14:09:36 -0400

    Initial Commit

---
+++ Backgammon Teacher/Backgammon_TeacherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Backgammon_TeacherApp.swift
+//  Backgammon Teacher
+//
+//  Created by Ovunc Ozgun Eker on 5/23/26.
+//
+
+import SwiftUI
+
+@main
+struct Backgammon_TeacherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
